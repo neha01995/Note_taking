848b0ec9a204c582e817f2b498e7ec683dcce50f
Neha Singla <nehasingla7180@gmail.com>
2020-05-26 22:57:35 +0530

    successfully completed note making api

--- controllers/notes_controller.js
+++ controllers/notes_controller.js
@@ -3,3 +3,124 @@ const Note = require('../models/Schema');
 module.exports.home = function(req, res){
     return res.json({"message":"Welcome to EasyNotes application.Takes notes quickly"});
 }
+
+// Create to save a new note
+module.exports.create = function(req, res){
+
+    // Validaterequest
+    if(!req.body.content){
+        return res.status(400).send({
+            message:"Notes content cannot be exmpty"
+        });
+    }
+
+    // Create a new note
+    const note = new Note({
+        title: req.body.title || "Untitle Note",
+        content: req.body.content
+        
+    });
+    
+    // Save note in databse
+    note.save()
+    .then(data => {
+        res.send(data);
+    }).catch(err => {
+        res.status(500).send({
+            message: err.message || "Some error while creating note"
+        });
+    });
+
+}
+
+
+// Retrieve and return all notes from the database.
+module.exports.findAll = function(req, res){
+    Note.find()
+    .then(notes => {
+        res.send(notes);
+    }).catch(err => {
+        res.status(500).send({
+            message: err.message || "Some error occurred while retrieving notes."
+        });
+    });
+}
+
+// Find a single note with a note id
+module.exports.findOne = function(req, res){
+    Note.findById(req.params.noteId)
+    .then(note => {
+        if(!note){
+            return res.status(404).send({
+                message: "Note not found with id " + req.params.noteId
+            });
+        }
+        res.send(note);
+    }).catch(err => {
+        if(err.kind === 'ObjectId'){
+            return res.status(404).send({
+                message: "Note not found with id " + req.params.noteId
+            });                
+        }
+        return res.status(500).send({
+            message: "Error reterieving note with id " + req.params.noteId
+        });
+    });
+}
+
+// Update a note identified by the noteid in the request
+module.exports.update = function(req, res){
+    // Validate request
+    if(!req.body.content){
+        return res.status(400).send({
+            message: "Note content can not be empty"
+        });
+    }
+
+    // Find note and update it with the request body
+    Note.findByIdAndUpdate(req.params.noteId, {
+        title: req.body.title || "Untitle Note",
+        content: req.body.content
+    }, {new: true})
+    .then(note => {
+        if(!note){
+            return res.status(404).send({
+                message: "Note not found with id " + req.params.noteId
+            });
+        }
+        res.send(note);
+    }).catch(err => {
+        if(err.kind === 'ObjectId') {
+            return res.status(404).send({
+                message: "Note not found with id " + req.params.noteId
+            });                
+        }
+        return res.status(500).send({
+            message: "Error updating note with id " + req.params.noteId
+        });
+    });
+    // The {new: true} option in the findByIdAndUpdate() method is used to return the modified document to the then() function instead of the original.
+}
+
+// Deleteing a note
+// Delete a note with the specified noteid in the request
+module.exports.delete = function(req, res){
+    Note.findByIdAndRemove(req.params.noteId)
+    .then(note => {
+        if(!note) {
+            return res.status(404).send({
+                message: "Note not found with id " + req.params.noteId
+            });
+        }
+        res.send({message: "Note deleted successfully!"});
+    }).catch(err => {
+        if(err.kind === 'ObjectId' || err.name === 'NotFound') {
+            return res.status(404).send({
+                message: "Note not found with id " + req.params.noteId
+            });                
+        }
+        return res.status(500).send({
+            message: "Could not delete note with id " + req.params.noteId
+        });
+    });
+}
